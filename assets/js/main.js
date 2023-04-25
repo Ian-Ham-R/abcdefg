bc88f9903f69923906568f5856f2689268a38245
Ian-Ham-R <111908304+Ian-Ham-R@users.noreply.github.com>
2023-04-24 19:10:39 -0600

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,18 @@
 const body = document.body,
     scrollWrap = document.getElementsByClassName("smooth-scroll-wrapper")[0],
-    height = scrollWrap.getBoundingClientRect().height - 1,
-    speed = 0.04;
+    height = scrollWrap.getBoundingClientRect().height - 1;
 
 var offset = 0;
 
 body.style.height = Math.floor(height) + "px";
 
 function smoothScroll() {
-    offset += (window.pageYOffset - offset) * speed;
+    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+        offset += (window.pageYOffset - offset) * 1;
+    }
+    else {
+        offset += (window.pageYOffset - offset) * 0.04;
+    }
 
     var scroll = "translateY(-" + offset + "px) translateZ(0)";
     scrollWrap.style.transform = scroll;
